b5fa4b74709fbd1eace6fc41ba98fb8cfcf9e610
2025 <satish.arya2025@outlook.com>
2025-09-11 22:28:55 +0530

    init commit

---
+++ src/HelloWorldPackage/InfyQ1.java
@@ -0,0 +1,13 @@
+package HelloWorldPackage;
+
+public class InfyQ1 {
+    public static void main(String[] args) {
+        int i= 10;
+        int j= i++;
+
+        System.out.println(i);
+        System.out.println(j);
+
+
+    }
+}
